4fe27abc7f10baf2dfa44ee66d9188464a9c018f
Chetan Shukla <chetanshukla95@gmail.com>
2017-09-07 18:54:39 +0530

    Create Dungeon_Princess.cpp

---
+++ Dynamic Programming/Dungeon_Princess.cpp
@@ -0,0 +1,38 @@
+int Solution::calculateMinimumHP(vector<vector<int> > &grid) 
+{
+    int row = grid.size();
+    int col = grid[0].size();
+    
+    vector< vector<int> > res;
+    res.resize(row);
+    
+    int i,j,ret = 0;
+    
+    for(i=0; i<row; i++)
+    {
+        res[i].resize(col,0);
+    }
+                                
+    res[row-1][col-1] = max(1,1-grid[row-1][col-1]);
+    
+    for(i=row-2; i>=0; i--)
+    {
+        res[i][col-1] = max(1,res[i+1][col-1] - grid[i][col-1]);
+    }
+    for(i=col-2; i>=0; i--)
+    {
+        res[row-1][i] = max(1,res[row-1][i+1] - grid[row-1][i]);
+    }
+    
+    for(i=row-2; i>=0; i--)
+    {
+        for(j=col-2; j>=0; j--)
+        {
+            res[i][j] = min(res[i+1][j], res[i][j+1]) - grid[i][j];
+            res[i][j] = max(res[i][j], 1);
+        }
+    }
+    ret = res[0][0];
+    
+    return ret;
+}
